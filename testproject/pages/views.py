9b89f7df722bf674b6c70abf4a7bd90e025b5b40
DimaGershchuk <dimagersuk29@gmail.com>
2024-08-12 21:05:32 +0300

    Created views and templates

--- testproject/pages/views.py
+++ testproject/pages/views.py
@@ -3,8 +3,12 @@ from django.shortcuts import render, get_object_or_404
 from .models import Book, Category, Publisher, Author
 
 
+def get_sidebar_authors():
+    return Author.objects.all()
+
+
 def home_view(request):
-    return render(request, 'home_page.html')
+    return render(request, 'home_page.html', {'all_authors': get_sidebar_authors()})
 
 
 def author_list(request):
@@ -15,19 +19,19 @@ def author_list(request):
 def author_books_view(request, author_id):
     author = get_object_or_404(Author, pk=author_id)
     books = Book.objects.filter(author=author)
-    return render(request, 'author_books.html', {'Author': author, 'Books': books})
+    return render(request, 'author_books.html', {'Author': author, 'Books': books, 'all_authors': get_sidebar_authors()})
 
 
 def books_view(request):
     books = Book.objects.filter(rating__gte=1)
-    return render(request, "book_list.html", {'Books': books})
+    return render(request, "book_list.html", {'Books': books, 'all_authors': get_sidebar_authors()})
 
 
 def category_view(request):
     categories = Category.objects.all()
-    return render(request, "category_list.html", {'Categories': categories})
+    return render(request, "category_list.html", {'Categories': categories, 'all_authors': get_sidebar_authors()})
 
 
 def publisher_view(request):
     publishers = Publisher.objects.all()
-    return render(request, 'publisher_list.html', {'Publishers': publishers})
+    return render(request, 'publisher_list.html', {'Publishers': publishers, 'all_authors': get_sidebar_authors()})
